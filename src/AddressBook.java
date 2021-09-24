811688536bbbabb8a3c4066f38d3e984f1dcb4b0
currytrash <91301277+currytrash@users.noreply.github.com>
2021-09-23 23:01:26 -0400

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -18,7 +18,7 @@ public class AddressBook {
 
     public static void main (String[] args){
         System.out.println("Address Book");
-        BuddyInfo buddy = new BuddyInfo("yahya" ,"carleton","613");
+        BuddyInfo buddy = new BuddyInfo("john" ,"carleton","613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
